3f3642749d0a342cb372238f0bcc6de7ab95ccd2
Shafeeq Duddiyanda <shafeeqsha49@gmail.com>
2025-06-12 06:19:02 +0000

    it is still there make search bar on top of navigation bar

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -228,7 +228,7 @@ const HomeScreen = () => {
           "transition-all duration-300 ease-in-out z-30",
           isSearchMode
             ? "bg-card shadow-md sticky top-0"
-            : "absolute bottom-0 left-0 right-0 px-4 py-2"
+            : "absolute bottom-0 left-0 right-0 px-4 pt-2 pb-0" // Changed py-2 to pt-2 pb-0
         )}
       >
         {!isSearchMode && renderSuggestions && (
