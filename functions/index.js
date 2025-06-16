fe13938b716880b1916f20c89e020ecf3db46413
Shafeeq Duddiyanda <shafeeqsha49@gmail.com>
2025-06-16 12:23:09 +0000

    === Deploying to 'locality-hub-wseoy'...
    i  deploying firestore, functi

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,16 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * const {onCall} = require("firebase-functions/v2/https");
+ * const {onCall} = require("firebase-functions/v2/call");
  * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
  *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * See a full list of supported triggers at
+ * https://firebase.google.com/docs/functions
  */
 
-// const {onRequest} = require("firebase-functions/v2/https"); // Removed as unused
-// const logger = require("firebase-functions/logger"); // Removed as unused
+// Example of requiring modules (currently unused):
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
