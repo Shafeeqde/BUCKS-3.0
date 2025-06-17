1435bd63409f0e5d2f63a01b68bc7b074a7958f5
Shafeeq Duddiyanda <shafeeqsha49@gmail.com>
2025-06-17 09:06:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/dummy-data/feedsCategories.ts
+++ src/lib/dummy-data/feedsCategories.ts
@@ -4,7 +4,7 @@ import { PlusIcon } from '@heroicons/react/24/outline';
 
 export const initialCategoriesData: Category[] = [
   { id: 'moments-0', name: 'Your Moments', icon: PlusIcon, type: 'moments', viewed: false, color: 'bg-primary/10 text-primary' },
-  { id: 'cat-deepthi', name: 'Deepthi', image: 'https://source.unsplash.com/random/100x100/?woman,portrait,indian', dataAiHint: 'woman portrait indian', viewed: false, profileId: 'deepthi-profile' },
+  { id: 'cat-deepthi', name: 'Deepthi', image: 'https://images.unsplash.com/photo-1602233158242-3ba0ac4d2167?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxHaXJsfGVufDB8fHx8MTc1MDE1MTE2OXww&ixlib=rb-4.1.0&q=80&w=1080', dataAiHint: 'woman portrait indian', viewed: false, profileId: 'deepthi-profile' },
   { id: 'cat-maanisha', name: 'Maanisha', image: 'https://source.unsplash.com/random/100x100/?woman,smiling,professional', dataAiHint: 'woman smiling professional', viewed: false, profileId: 'manisha-profile' },
   { id: 'cat-subhesh', name: 'Subhesh', image: 'https://source.unsplash.com/random/100x100/?man,office,indian', dataAiHint: 'man office indian', viewed: true, profileId: 'subhesh-profile' },
   { id: 'cat-seena', name: 'Seena', image: 'https://source.unsplash.com/random/100x100/?person,outdoor,female', dataAiHint: 'person outdoor female', viewed: false, profileId: 'seena-profile' },
